439b205c04cda0c26227d4645f59e4e5a285ca53
caesar-augustus <caesar.augustusqp@gmail.com>
2023-03-20 20:57:48 +0100

    eliminado cubo interior innecesario

--- js/rubik.js
+++ js/rubik.js
@@ -1,32 +1,30 @@
 let scene, camera, renderer;
 
 let cube000, cube001, cube002, cube010, cube011, cube012, cube020, cube021, cube022;
-let cube100, cube101, cube102, cube110, cube111, cube112, cube120, cube121, cube122;
+let cube100, cube101, cube102, cube110, cube112, cube120, cube121, cube122;
 let cube200, cube201, cube202, cube210, cube211, cube212, cube220, cube221, cube222;
 let rubik;
 let selectedFace;
 
 function init() {
+    // Crea y configura la escena
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+    // Crea y configura el renderer
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(new THREE.Color(0xffffff), 1.0);
     document.body.appendChild( renderer.domElement );
     
-    rubik = createRubik(3)
-    selectedFace = new THREE.Group();
-
-    createRubik(rubik);
-    scene.add(rubik);
-
+    // Crea y configura la camara asi como sus controles
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
     camera.position.z = 5;
 }
 
 function update() {
-    rubik.rotation.x += 0.01;
-    rubik.rotation.y += 0.01;
-    rubik.rotation.z += 0.01;
+    //rubik.rotation.x += 0.01;
+    //rubik.rotation.y += 0.01;
+    //rubik.rotation.z += 0.01;
 }
 
 function render() {
@@ -37,10 +35,7 @@ function render() {
     renderer.render( scene, camera );
 }
 
-function createRubik(dimensions) {
-    let i, j, k;
-    //let max = dimensions / 2;
-    //let min = -1 * max;
+function addRubik() {
     let min = 0;
     let max = 3;
     
@@ -49,9 +44,10 @@ function createRubik(dimensions) {
     const geometry = new THREE.BoxGeometry(.9, .9, .9, 1, 1, 1 );
     const material = new THREE.LineBasicMaterial( { color: 0x00ff00 } );
 
-    for (i = min; i < max; i++) {
-        for (j = min; j < max; j++) {
-            for (k = min; k < max; k++) {
+    for (let i = min; i < max; i++) {
+        for (let j = min; j < max; j++) {
+            for (let k = min; k < max; k++) {
+                if (i == 1 &  j == 1 & k == 1) { break; }
                 eval("cube" + i + j + k + " = new THREE.Mesh( geometry, material);");
                 eval("cube" + i + j + k + ".position.set(i - 1, j - 1, k - 1)");
                 eval("rubik.add(cube" + i + j + k + ");");
@@ -60,8 +56,9 @@ function createRubik(dimensions) {
         }
     }
 
-    return rubik;
+    scene.add(rubik);
 }
 
 init();
+addRubik();
 render();

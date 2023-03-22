ffd75fec7779e215787d34cfd682c3488d3eb07c
caesar-augustus <caesar.augustusqp@gmail.com>
2023-03-22 01:55:00 +0100

    Creado listener onMouseMove

--- js/rubik.js
+++ js/rubik.js
@@ -1,9 +1,5 @@
 let scene, camera, renderer;
-let cameraControl;
-
-let cube000, cube001, cube002, cube010, cube011, cube012, cube020, cube021, cube022;
-let cube100, cube101, cube102, cube110, cube112, cube120, cube121, cube122;
-let cube200, cube201, cube202, cube210, cube211, cube212, cube220, cube221, cube222;
+let cameraControl, mouse, raycaster;
 
 let rubik;
 let selectedFace;
@@ -16,7 +12,6 @@ function init() {
     // Crea y configura el renderer
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    //renderer.setClearColor(new THREE.Color(0xffffff), 1.0);
     document.body.appendChild(renderer.domElement);
     
     // Crea y configura la camara asi como sus controles
@@ -25,9 +20,16 @@ function init() {
     camera.position.y = 1;
     camera.position.z = 7;
 
+    // Controles
     cameraControl = new THREE.OrbitControls(camera, renderer.domElement);
     cameraControl.target.set(1, 1, 1);
     camera.lookAt(1, 1, 1); 
+
+    mouse = new THREE.Vector2();
+    raycaster = new THREE.Raycaster();
+
+    // Listeners
+    window.addEventListener('mousemove', onMouseMove);
 }
 
 function update() {
@@ -45,20 +47,17 @@ function render() {
 }
 
 function addRubik() {
-    let min = 0;
-    let max = 3;
-    
     rubik = new THREE.Group();
 
-    let cuboBase = createBaseCube();
+    let baseCube = createBaseCube();
 
-    for (let i = min; i < max; i++) {
-        for (let j = min; j < max; j++) {
-            for (let k = min; k < max; k++) {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            for (let k = 0; k < 3; k++) {
                 if (i == 1 && j == 1 && k == 1) { continue; }
-                eval("cube" + i + j + k + " = cuboBase.clone();");
-                eval("cube" + i + j + k + ".position.set(i, j, k)");
-                eval("rubik.add(cube" + i + j + k + ");");
+                let clone = baseCube.clone();
+                clone.position.set(i, j, k);
+                rubik.add(clone);
             }
         }
     }
@@ -68,46 +67,61 @@ function addRubik() {
 function addSkybox() {
     let geometry = new THREE.BoxGeometry(100, 100, 100);
 
-    let ft = new THREE.TextureLoader().load("img/corona_ft.png");
-    let bk = new THREE.TextureLoader().load("img/corona_bk.png");
+    let front = new THREE.TextureLoader().load("img/corona_ft.png");
+    let back = new THREE.TextureLoader().load("img/corona_bk.png");
     let up = new THREE.TextureLoader().load("img/corona_up.png");
-    let dn = new THREE.TextureLoader().load("img/corona_dn.png");
-    let rt = new THREE.TextureLoader().load("img/corona_rt.png");
-    let lf = new THREE.TextureLoader().load("img/corona_lf.png");
+    let down = new THREE.TextureLoader().load("img/corona_dn.png");
+    let right = new THREE.TextureLoader().load("img/corona_rt.png");
+    let left = new THREE.TextureLoader().load("img/corona_lf.png");
 
     let materials = [
-        new THREE.MeshBasicMaterial({map: ft, side: THREE.BackSide}) ,
-        new THREE.MeshBasicMaterial({map: bk, side: THREE.BackSide}) ,
+        new THREE.MeshBasicMaterial({map: front, side: THREE.BackSide}) ,
+        new THREE.MeshBasicMaterial({map: back, side: THREE.BackSide}) ,
         new THREE.MeshBasicMaterial({map: up, side: THREE.BackSide}) ,
-        new THREE.MeshBasicMaterial({map: dn, side: THREE.BackSide}) ,
-        new THREE.MeshBasicMaterial({map: rt, side: THREE.BackSide}) ,
-        new THREE.MeshBasicMaterial({map: lf, side: THREE.BackSide})
+        new THREE.MeshBasicMaterial({map: down, side: THREE.BackSide}) ,
+        new THREE.MeshBasicMaterial({map: right, side: THREE.BackSide}) ,
+        new THREE.MeshBasicMaterial({map: left, side: THREE.BackSide})
     ]
-
 
     let skybox = new THREE.Mesh(geometry, materials);
     scene.add(skybox);
-    console.log("Skybox añadida");
+}
+
+function onMouseMove(event) {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+
+    let intersects = raycaster.intersectObjects(scene.children);
+    // mayor que 1 ya que el skybox siempre va a estar seleccionado
+    if(intersects.length > 1) {
+        console.log(intersects[0].object.position)
+    }
+}
+
+function onMouseDown() {
+    
 }
 
 function createBaseCube(cubo) {
     let geometry = new THREE.BoxGeometry(.95, .95, .95).toNonIndexed();
 
-    // blanco, amarillo, naranja, rojo, verde, azul
-    const materialFront = new THREE.MeshPhongMaterial({ color: 0xffffff }); 
-    const materialBack = new THREE.MeshPhongMaterial({ color: 0xffffad });
-    const materialTop = new THREE.MeshPhongMaterial({ color: 0xffd493 });
-    const materialBottom = new THREE.MeshPhongMaterial({ color: 0xff9f8c });
-    const materialLeft = new THREE.MeshPhongMaterial({ color: 0x9affff });
-    const materialRight = new THREE.MeshPhongMaterial({ color: 0xa9bcff }); 
+    // blanco, amarillo, naranja, rojo, verde, azul en colores pastel
+    let front = new THREE.MeshPhongMaterial({ color: 0xffffff }); 
+    let back = new THREE.MeshPhongMaterial({ color: 0xffffad });
+    let up = new THREE.MeshPhongMaterial({ color: 0xffd493 });
+    let down = new THREE.MeshPhongMaterial({ color: 0xff9f8c });
+    let right = new THREE.MeshPhongMaterial({ color: 0xa9bcff }); 
+    let left = new THREE.MeshPhongMaterial({ color: 0x9affff });
 
     let materials = [
-        materialFront,
-        materialBack,
-        materialTop,
-        materialBottom,
-        materialLeft,
-        materialRight
+        front,
+        back,
+        up,
+        down,
+        right,
+        left
     ]
 
     let cube = new THREE.Mesh(geometry, materials)
